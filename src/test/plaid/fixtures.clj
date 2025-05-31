2c65cec667b1d77a6a707e39ce7b55e19feb81eb
Luke Gessler <lukegessler@gmail.com>
2025-05-31 12:01:56 -0400

    refactor

--- src/test/plaid/fixtures.clj
+++ src/test/plaid/fixtures.clj
@@ -84,3 +84,62 @@
   (-> (mock/request method path)
       (mock/header "accept" "application/edn")
       (mock/header "Authorization" (str "Bearer " user2-token))))
+
+;; API Testing Helper Functions
+(defn parse-response-body 
+  "Parse the response body from EDN format"
+  [response]
+  (read-string (slurp (:body response))))
+
+(defn api-call
+  "Make an API call using a request map with :method, :path, and optional :body"
+  [user-request-fn request-map]
+  (let [{:keys [method path body]} request-map
+        req (cond-> (user-request-fn method path)
+              body (mock/json-body body))
+        resp (rest-handler req)]
+    {:status (:status resp)
+     :body (when-let [response-body (:body resp)]
+             (when-not (= response-body "")
+               (parse-response-body resp)))}))
+
+;; Assertion Helpers
+(defn assert-status 
+  "Assert that response has expected status code"
+  [expected-status response]
+  (is (= expected-status (:status response))))
+
+(defn assert-success 
+  "Assert that response has 2xx status code"
+  [response]
+  (is (< 199 (:status response) 300)))
+
+(defn assert-created 
+  "Assert that response has 201 Created status"
+  [response]
+  (assert-status 201 response))
+
+(defn assert-ok 
+  "Assert that response has 200 OK status"
+  [response]
+  (assert-status 200 response))
+
+(defn assert-no-content 
+  "Assert that response has 204 No Content status"
+  [response]
+  (assert-status 204 response))
+
+(defn assert-not-found 
+  "Assert that response has 404 Not Found status"
+  [response]
+  (assert-status 404 response))
+
+(defn assert-forbidden 
+  "Assert that response has 403 Forbidden status"
+  [response]
+  (assert-status 403 response))
+
+(defn assert-bad-request 
+  "Assert that response has 400 Bad Request status"
+  [response]
+  (assert-status 400 response))
