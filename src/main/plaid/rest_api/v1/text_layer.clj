bbc4051a8ade534e16439b7dd0cd07ce8e3262de
Luke Gessler <lukegessler@gmail.com>
2025-06-02 10:16:03 -0400

    encapsulate admin, jwt->user-idg

---
+++ src/main/plaid/rest_api/v1/text_layer.clj
@@ -0,0 +1,65 @@
+(ns plaid.rest-api.v1.text-layer
+  (:require [plaid.rest-api.v1.auth :as pra]
+            [reitit.coercion.malli]
+            [plaid.xtdb.text-layer :as txtl]
+            [plaid.xtdb.common :as pxc]))
+
+(def text-layer-routes
+  ["/projects/:project-id/text-layers"
+   {:middleware [[pra/wrap-maintainer-required :project-id]]}
+
+   [""
+    {:post {:summary    "Create a new text layer for a project."
+            :parameters {:path [:map [:project-id :uuid]]
+                         :body [:map
+                                [:name :string]
+                                [:config {:optional true} map?]]}
+            :handler    (fn [{{{:keys [project-id]} :path {:keys [name config]} :body} :parameters xtdb :xtdb :as req}]
+                          (let [attrs {:text-layer/name   name
+                                       :text-layer/config (or config {})}]
+                            (let [result (txtl/create {:node xtdb} attrs project-id)]
+                              (if (:success result)
+                                {:status 201
+                                 :body   {:id (:extra result)}}
+                                {:status (or (:code result) 500)
+                                 :body   {:error (:error result)}}))))}}]
+
+   ["/:text-layer-id"
+    {:parameters {:path [:map [:project-id :uuid] [:text-layer-id :string]]}}
+
+    [:get {:summary "Get a text layer by ID."
+           :handler (fn [{{{:keys [project-id text-layer-id]} :path} :parameters xtdb :xtdb}]
+                      (let [text-layer (txtl/get xtdb text-layer-id)]
+                        (if (some? text-layer)
+                          {:status 200
+                           :body   (dissoc text-layer :xt/id)}
+                          {:status 404
+                           :body   {:error "Text layer not found"}})))}]
+
+    [:patch {:summary    "Update a text layer's name."
+             :parameters {:body [:map [:name :string]]}
+             :handler    (fn [{{{:keys [project-id text-layer-id]} :path {:keys [name]} :body} :parameters xtdb :xtdb}]
+                           (let [{:keys [success code error]} (txtl/merge {:node xtdb} text-layer-id {:text-layer/name name})]
+                             (if success
+                               {:status 200
+                                :body   (dissoc (txtl/get xtdb text-layer-id) :xt/id)}
+                               {:status (or code 404)
+                                :body   {:error (or error "Failed to update text layer or text layer not found")}})))}]
+    [:delete {:summary "Delete a text layer."
+              :handler (fn [{{{:keys [project-id text-layer-id]} :path} :parameters xtdb :xtdb}]
+                         (let [{:keys [success code error]} (txtl/delete {:node xtdb} text-layer-id)]
+                           (if success
+                             {:status 204}
+                             {:status (or code 404)
+                              :body   {:error (or error "Text layer not found")}})))}]
+
+    ["/shift"
+     {:post {:summary    "Shift a text layer's order within the project."
+             :parameters {:body [:map [:direction [:enum "up" "down"]]]}
+             :handler    (fn [{{{:keys [project-id text-layer-id]} :path {:keys [direction]} :body} :parameters xtdb :xtdb}]
+                           (let [up? (= direction "up")
+                                 {:keys [success code error]} (txtl/shift-text-layer {:node xtdb} project-id text-layer-id up?)]
+                             (if success
+                               {:status 204}
+                               {:status (or code 400)
+                                :body   {:error (or error "Failed to shift text layer")}})))}}]]]) 
