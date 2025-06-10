d8c29f077016a392e4f0344694afff6d2c6ec6c5
Luke Gessler <lukegessler@gmail.com>
2025-06-10 11:59:56 -0400

    fix refactor error

--- src/main/plaid/rest_api/v1/layer.clj
+++ src/main/plaid/rest_api/v1/layer.clj
@@ -3,22 +3,23 @@
             [plaid.xtdb.project :as prj]))
 
 (defn layer-config-routes [id-keyword]
-  ["/config/:editor-name/:config-key"
+  ["/config/:namespace/:config-key"
    {:middleware [[pra/wrap-writer-required #(-> % :parameters :path id-keyword)]]
-    :put        {:summary    "Set a configuration value for a layer in a specific editor namespace"
-                 :parameters {:path [:map [id-keyword :uuid] [:editor-name string?] [:config-key string?]]
+    :put        {:summary    (str "Set a configuration value for a layer in a editor namespace. "
+                                  "Good for storing data useful for ")
+                 :parameters {:path [:map [id-keyword :uuid] [:namespace string?] [:config-key string?]]
                               :body any?}
-                 :handler    (fn [{{{:keys [editor-name config-key] id id-keyword} :path config-value :body} :parameters xtdb :xtdb}]
-                               (let [{:keys [success code error]} (prj/assoc-editor-config-pair {:node xtdb} id editor-name config-key config-value)]
+                 :handler    (fn [{{{:keys [namespace config-key] id id-keyword} :path config-value :body} :parameters xtdb :xtdb}]
+                               (let [{:keys [success code error]} (prj/assoc-editor-config-pair {:node xtdb} id namespace config-key config-value)]
                                  (if success
                                    {:status 204}
                                    {:status (or code 400)
                                     :body   {:error error}})))}
 
-    :delete     {:summary    "Remove a configuration value for a layer in a specific editor namespace"
-                 :parameters {:path [:map [:id :uuid] [:editor-name string?] [:config-key string?]]}
-                 :handler    (fn [{{{:keys [editor-name config-key] id id-keyword} :path} :parameters xtdb :xtdb}]
-                               (let [{:keys [success code error]} (prj/dissoc-editor-config-pair {:node xtdb} id editor-name config-key)]
+    :delete     {:summary    "Remove a configuration value for a layer in a editor namespace"
+                 :parameters {:path [:map [:id :uuid] [:namespace string?] [:config-key string?]]}
+                 :handler    (fn [{{{:keys [namespace config-key] id id-keyword} :path} :parameters xtdb :xtdb}]
+                               (let [{:keys [success code error]} (prj/dissoc-editor-config-pair {:node xtdb} id namespace config-key)]
                                  (if success
                                    {:status 204}
                                    {:status (or code 400)
