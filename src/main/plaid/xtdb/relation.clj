75b554b5b6747a1cec461e05460cdb0e8d695aa2
Luke Gessler <lukegessler@gmail.com>
2025-06-30 10:16:36 -0400

    cleanup of schema checking

--- src/main/plaid/xtdb/relation.clj
+++ src/main/plaid/xtdb/relation.clj
@@ -62,6 +62,53 @@
           span-id)))
 
 ;; Mutations --------------------------------------------------------------------------------
+(defn- check-relation-invariants!
+  "Check invariants for a relation between source and target spans.
+  Validates that:
+  - Both spans exist
+  - Spans are in the same layer
+  - Spans are in the same document  
+  - Relation layer is linked to span layer"
+  [db {:relation/keys [layer source target]} source-record target-record]
+  ;; Source span exists?
+  (when-not (:span/id source-record)
+    (throw (ex-info (str "Source span " source " does not exist") {:id source :code 400})))
+  
+  ;; Target span exists?
+  (when-not (:span/id target-record)
+    (throw (ex-info (str "Target span " target " does not exist") {:id target :code 400})))
+  
+  ;; Source and target spans in same layer?
+  (when-not (= (:span/layer source-record) (:span/layer target-record))
+    (throw (ex-info "Source and target relations must be contained in a single span layer."
+                    {:source-layer (:span/layer source-record)
+                     :target-layer (:span/layer target-record)
+                     :code         400})))
+  
+  ;; Relation layer linked to span layer?
+  (when-not ((set (:span-layer/relation-layers (pxc/entity db (:span/layer source-record)))) layer)
+    (throw (ex-info (str "Relation layer " layer " is not connected to span layer " (:span/layer source-record))
+                    {:relation-layer layer :span-layer (:span/layer source-record) :code 400})))
+  
+  ;; Source and target spans in same doc?
+  (when-not (= (get-doc-id-of-span db source) (get-doc-id-of-span db target))
+    (throw (ex-info "Source and target relations must be in a single document."
+                    {:source-document (get-doc-id-of-span db source)
+                     :target-document (get-doc-id-of-span db target)
+                     :code            400}))))
+
+(defn- check-relation-exists!
+  "Check if a relation exists. Throws exception if not."
+  [db eid]
+  (when-not (:relation/id (pxc/entity db eid))
+    (throw (ex-info (pxc/err-msg-not-found "Relation" eid) {:code 404 :id eid}))))
+
+(defn- check-relations-consistency!
+  "Check if all relations belong to same layer and document.
+  Used for bulk operations."
+  [relations-attrs]
+  (when-not (= 1 (->> relations-attrs (map :relation/layer) distinct count))
+    (throw (ex-info "Relations must all belong to the same layer" {:code 400}))))
 (defn- relation-attr? 
   "Check if an attribute key belongs to relation namespace (including metadata attributes)."
   [k]
@@ -75,49 +122,24 @@
                                                                             (into {} relation-attrs))
         source-record (pxc/entity db source)
         target-record (pxc/entity db target)]
-
-    (cond
-      ;; ID is not already taken?
-      (some? (pxc/entity db id))
-      (throw (ex-info (pxc/err-msg-already-exists "Relation" id) {:id id :code 409}))
-
-      ;; Relation layer exists?
-      (not (:relation-layer/id (pxc/entity db layer)))
-      (throw (ex-info (pxc/err-msg-not-found "Relation layer" layer) {:id layer :code 400}))
-
-      ;; Source span exists?
-      (not (:span/id source-record))
-      (throw (ex-info (str "Source span " source " does not exist") {:id source :code 400}))
-
-      ;; Target span exists?
-      (not (:span/id target-record))
-      (throw (ex-info (str "Source span " target " does not exist") {:id target :code 400}))
-
-      ;; Source and target spans in same layer?
-      (not= (:span/layer source-record) (:span/layer target-record))
-      (throw (ex-info "Source and target relations must be contained in a single span layer."
-                      {:source-layer (:span/layer source-record)
-                       :target-layer (:span/layer target-record)
-                       :code         400}))
-
-      ;; Relation layer linked to span layer?
-      (not ((set (:span-layer/relation-layers (pxc/entity db (:span/layer source-record)))) layer))
-      (throw (ex-info (str "Relation layer " layer " is not connected to span layer " (:span/layer source-record))
-                      {:relation-layer layer :span-layer (:span/layer source-record) :code 400}))
-
-      ;; Source and target spans in same doc?
-      (not= (get-doc-id-of-span db source) (get-doc-id-of-span db target))
-      (throw (ex-info "Source and target relations must be in a single document."
-                      {:source-document (get-doc-id-of-span db source)
-                       :target-document (get-doc-id-of-span db target)
-                       :code            400}))
-
-      :else
-      [[::xt/match id nil]
-       [::xt/match source source-record]
-       [::xt/match target target-record]
-       [::xt/match layer (pxc/entity db layer)]
-       [::xt/put r]])))
+    
+    ;; ID is not already taken?
+    (when (some? (pxc/entity db id))
+      (throw (ex-info (pxc/err-msg-already-exists "Relation" id) {:id id :code 409})))
+    
+    ;; Relation layer exists?
+    (when-not (:relation-layer/id (pxc/entity db layer))
+      (throw (ex-info (pxc/err-msg-not-found "Relation layer" layer) {:id layer :code 400})))
+    
+    ;; Check all relation invariants
+    (check-relation-invariants! db r source-record target-record)
+    
+    ;; Return transaction operations
+    [[::xt/match id nil]
+     [::xt/match source source-record]
+     [::xt/match target target-record]
+     [::xt/match layer (pxc/entity db layer)]
+     [::xt/put r]]))
 
 (defn create-operation
   "Build an operation for creating a relation"
@@ -170,50 +192,32 @@
   (let [{:keys [db node] :as xt-map} (pxc/ensure-db xt-map)
         {:relation/keys [id layer source target] :as r} (pxc/entity db eid)
         new-span (pxc/entity db span-id)
-        other-span (if (= :relation/target key)
-                     (pxc/entity db source)
-                     (pxc/entity db target))]
-    (cond
-      ;; Relation exists?
-      (nil? id)
-      (throw (ex-info (pxc/err-msg-not-found "Relation" eid) {:id eid :code 404}))
-
-      ;; Span exists?
-      (not (:span/id new-span))
-      (throw (ex-info (str "Span " span-id " does not exist") {:id span-id :code 400}))
-
-      ;; Relation layer linked to span layer?
-      (not ((set (:span-layer/relation-layers (pxc/entity db (:span/layer new-span)))) layer))
-      (throw (ex-info (str "Relation layer " layer " is not connected to span layer " (:span/layer new-span))
-                      {:relation-layer layer :span-layer (:span/layer new-span) :code 400}))
-
-      ;; Key is valid?
-      (not (#{:relation/source :relation/target} key))
+        other-span-id (if (= :relation/target key) source target)
+        other-span (pxc/entity db other-span-id)]
+    
+    ;; Relation exists?
+    (check-relation-exists! db eid)
+    
+    ;; Key is valid?
+    (when-not (#{:relation/source :relation/target} key)
       (throw (ex-info "Key must be either :relation/source or :relation/target"
-                      {:code 500 :key key}))
-
-      ;; Source and target spans in same doc?
-      (not= (get-doc-id-of-span db (:span/id new-span)) (get-doc-id-of-span db (:span/id other-span)))
-      (throw (ex-info "Source and target relations must be in a single document."
-                      {:source-document (get-doc-id-of-span db (:span/id source))
-                       :target-document (get-doc-id-of-span db (:span/id target))
-                       :code            400}))
-
-
-      ;; Spans in same layer?
-      (not= (:span/layer other-span) (:span/layer new-span))
-      (throw (ex-info "Source and target relations must be in the same layer."
-                      {:current-layer (:span/layer other-span)
-                       :new-layer     (:span/layer new-span)
-                       :code          400}))
-
-      :else
-      [[::xt/match id r]
-       [::xt/match source (pxc/entity db source)]
-       [::xt/match target (pxc/entity db target)]
-       [::xt/match layer (pxc/entity db layer)]
-       [::xt/match span-id new-span]
-       [::xt/put (assoc r key span-id)]])))
+                      {:code 500 :key key})))
+    
+    ;; Check relation invariants for the new configuration
+    (let [test-source (if (= :relation/source key) span-id source)
+          test-target (if (= :relation/target key) span-id target)
+          test-source-record (if (= :relation/source key) new-span (pxc/entity db source))
+          test-target-record (if (= :relation/target key) new-span (pxc/entity db target))
+          test-relation (assoc r :relation/source test-source :relation/target test-target)]
+      (check-relation-invariants! db test-relation test-source-record test-target-record))
+    
+    ;; Return transaction operations
+    [[::xt/match id r]
+     [::xt/match source (pxc/entity db source)]
+     [::xt/match target (pxc/entity db target)]
+     [::xt/match layer (pxc/entity db layer)]
+     [::xt/match span-id new-span]
+     [::xt/put (assoc r key span-id)]]))
 
 (defn set-end-operation
   "Build an operation for updating a relation's source or target"
@@ -237,10 +241,10 @@
 (defn delete* [xt-map eid]
   (let [{:keys [db node] :as xt-map} (pxc/ensure-db xt-map)
         r (pxc/entity db eid)]
-
-    (when-not (:relation/id (pxc/entity db eid))
-      (throw (ex-info (pxc/err-msg-not-found "Relation" eid) {:code 404 :id eid})))
-
+    
+    ;; Check relation exists
+    (check-relation-exists! db eid)
+    
     [[::xt/match eid r]
      [::xt/delete eid]]))
 
@@ -311,9 +315,8 @@
                                    (clojure.core/merge relation-attrs metadata-attrs))
                                  (dissoc attrs :metadata)))
                              relations-attrs)]
-    ;; Validate all relations are for the same layer
-    (when-not (= 1 (->> relations-attrs (map :relation/layer) distinct count))
-      (throw (ex-info "Relations must all belong to the same layer" {:code 400})))
+    ;; Check consistency of relations
+    (check-relations-consistency! relations-attrs)
 
     ;; Validate all relations belong to the same document (via their spans)
     (let [doc-ids (->> relations-attrs
@@ -335,44 +338,18 @@
                                                                                              (into {} relation-attrs))
                   source-record (pxc/entity db source)
                   target-record (pxc/entity db target)]
-              ;; Validate this relation's attributes (same validation as single create)
-              (cond
-                ;; ID is not already taken?
-                (some? (pxc/entity db id))
-                (throw (ex-info (pxc/err-msg-already-exists "Relation" id) {:id id :code 409}))
-
-                ;; Source span exists?
-                (not (:span/id source-record))
-                (throw (ex-info (str "Source span " source " does not exist") {:id source :code 400}))
-
-                ;; Target span exists?
-                (not (:span/id target-record))
-                (throw (ex-info (str "Target span " target " does not exist") {:id target :code 400}))
-
-                ;; Source and target spans in same layer?
-                (not= (:span/layer source-record) (:span/layer target-record))
-                (throw (ex-info "Source and target relations must be contained in a single span layer."
-                                {:source-layer (:span/layer source-record)
-                                 :target-layer (:span/layer target-record)
-                                 :code         400}))
-
-                ;; Relation layer linked to span layer?
-                (not ((set (:span-layer/relation-layers (pxc/entity db (:span/layer source-record)))) layer))
-                (throw (ex-info (str "Relation layer " layer " is not connected to span layer " (:span/layer source-record))
-                                {:relation-layer layer :span-layer (:span/layer source-record) :code 400}))
-
-                ;; Source and target spans in same doc?
-                (not= (get-doc-id-of-span db source) (get-doc-id-of-span db target))
-                (throw (ex-info "Source and target relations must be in a single document."
-                                {:source-document (get-doc-id-of-span db source)
-                                 :target-document (get-doc-id-of-span db target)
-                                 :code            400}))
-
-                :else
-                (into tx-ops [[::xt/match id nil]
-                              [::xt/match source source-record]
-                              [::xt/match target target-record]
-                              [::xt/put relation]]))))
+              ;; Validate this relation
+              (when (some? (pxc/entity db id))
+                (throw (ex-info (pxc/err-msg-already-exists "Relation" id) {:id id :code 409})))
+              
+              ;; Check relation invariants
+              (check-relation-invariants! db relation source-record target-record)
+              
+              ;; Add to transaction operations
+              (into tx-ops [[::xt/match id nil]
+                            [::xt/match source source-record]
+                            [::xt/match target target-record]
+                            [::xt/put relation]])))
           []
           relations-attrs)))))
 
