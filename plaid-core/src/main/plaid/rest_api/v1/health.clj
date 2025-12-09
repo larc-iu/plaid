0ba66505c96fd60f5f2b1aa164b2248174580e2e
PT Anderson <revitalizationtech@gmail.com>
2025-12-09 12:01:33 -0800

    add timestamp

--- plaid-core/src/main/plaid/rest_api/v1/health.clj
+++ plaid-core/src/main/plaid/rest_api/v1/health.clj
@@ -1,8 +1,10 @@
-(ns plaid.rest-api.v1.health)
+(ns plaid.rest-api.v1.health
+  (:import [java.time Instant]))
 
 (def health-routes
   [["/health"
     {:get {:summary "Health check endpoint"
            :handler (fn [_]
                       {:status 200
-                       :body   {:status "healthy"}})}}]])
+                       :body   {:status    "healthy"
+                                :timestamp (str (Instant/now))}})}}]])
