a13d9313d017a95fd078d89bdec65ad0f3032768
Luke Gessler <lukegessler@gmail.com>
2026-06-03 16:03:37 -0400

    test-runner: support multiple --namespace flags in one run
    Pass --namespace repeatedly to run several test namespaces in a single
    invocation (accumulated into :nss, required + find-tests via mapcat).
    Backward-compatible with the singular path.

--- plaid-core/src/test-runner/plaid/test_runner.clj
+++ plaid-core/src/test-runner/plaid/test_runner.clj
@@ -7,12 +7,14 @@
                  opts
                  (case (first args)
                    "--namespace" (recur (drop 2 args)
-                                        (assoc opts :ns (symbol (second args))))
+                                        (update opts :nss (fnil conj []) (symbol (second args))))
                    "--var"       (recur (drop 2 args)
                                         (assoc opts :var (second args)))
                    ;; default: skip unknown
                    (recur (rest args) opts))))
         tests (cond
+                (:nss opts) (do (run! require (:nss opts))
+                                (vec (mapcat eftest/find-tests (:nss opts))))
                 (:ns opts)  (do (require (:ns opts))
                                 (eftest/find-tests (:ns opts)))
                 (:var opts) (do (require (symbol (namespace (symbol (:var opts)))))
